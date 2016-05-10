cf73e32bb833c9801d74f3f0320985415c279850
wangzhande <15313086535@163.com>
2016-05-10 16:35:31 +0800

    拼接图片路径

--- scsg/controllers/mobileGoods.php
+++ scsg/controllers/mobileGoods.php
@@ -4,20 +4,27 @@
 首页数据
 author:wangzhande
  */
-class mobileGoods extends IController {
+class mobieGoods extends IController {
 
 	//获取分类列表
-	public function getCategoryList() {
+	public function getAllCat() {
 		$m_category = new IModel('category');
 		$where = array(
 			'parent_id' => 0,
 		);
 		$c_list = $m_category->query();
-
-		//$result=$this->getTreeList($c_list);
 		$result = $this->getNestedList($c_list);
-
 		echo JSON::encode($result);
+	}
+	//获取顶级分类
+	public function getCatList() {
+		$m_category = new IQuery('category');
+		$m_category->where = 'parent_id=0';
+
+		$m_category->order = 'sort asc';
+
+		$cateList = $m_category->find();
+		echo JSON::encode($cateList);
 
 	}
 	//递归获取所有父类下子分类,树状形的
@@ -52,17 +59,6 @@ class mobileGoods extends IController {
 		return $arr;
 
 	}
-	//获取顶级分类
-	public function getCatList() {
-		$m_category = new IQuery('category');
-		$m_category->where = 'parent_id=0';
-		//$m_category->where='id=2';
-		$m_category->order = 'sort asc';
-
-		$cateList = $m_category->find();
-		echo JSON::encode($cateList);
-
-	}
 
 	//获取所有品牌
 	public function getBrandList() {
@@ -71,37 +67,74 @@ class mobileGoods extends IController {
 		echo JSON::encode($b_list);
 
 	}
-	//获得某个分类下的商品品牌
+	//获得所有商品品牌
 	public function getGoodsBrand() {
 		//取得品牌id
-		$id = IFilter::act(IReq::get('cat_id'), 'int');
-		$m_brand = new IModel('brand');
+		$id = IFilter::act(IReq::get('cat_id'));
+		$m_brand = new IModel('brand as b');
+		/*$m_brand->fileds=''*/
 		$result = $m_brand->query('category_ids like "%' . $id . '%"');
-
 		echo JSON::encode($result);
-
 	}
+	//获取最新，最热，商品
+	public function getHotGoods() {
+		$type = IFilter::act(IReq::get('type'));
+		//var_dump($type);
+		$m_goods = new IModel('goods');
+		$where = 'is_del=3';
+		$cols = '*';
+		switch ($type) {
+			case "hot":
+				$order = "sale";
+				break;
+			case "new":
+				$order = "up_time";
+				break;
+			default:
+				$order = "";
 
-	//团购数据
+		}
+		$desc = 'DESC';
+		$limit = 5;
+		$hotList = $m_goods->query($where, $cols, $order, $desc, $limit);
+		foreach ($hotList as $k => $v) {
+			$hotList[$k]['img'] = 'http://v.yqrtv.com:8080/app/' . $v['img'];
+		}
+
+		echo JSON::encode($hotList);
+	}
+	//首页团购数据
 	public function getRegiment() {
 		//实例化团购表
 		$m_regiment = new IModel('regiment');
-		/*$where=array(
-		'is_close'=>0
-		);*/
 		$where = 'is_close = 0 and NOW() between start_time and end_time';
-		$order = 'id desc';
+		$order = 'id';
 		//排序
 		//$order='sort';
 		//字段
 		$cols = '*';
 
 		$re_list = $m_regiment->query($where, $cols, $order);
-
-		//$result=$this->getTreeList($c_list);
+		foreach ($re_list as $k => $v) {
+			$re_list[$k]['img'] = 'http://v.yqrtv.com:8080/app/' . $v['img'];
+		}
 		echo JSON::encode($re_list);
 	}
-	//获得推荐商品
+	//特价商品
+	public function getTejia() {
+		$m_goods = new IQuery('goods as go');
+		$m_goods->fields = 'go.img,go.id,.go.name';
+		$m_goods->join = 'left join commend_goods  as c on go.id=c.goods_id';
+		$m_goods->where = 'c.commend_id=3';
+		$m_goods->order = 'sort DESC';
+		$m_goods->limit = '5';
+		$result = $m_goods->find();
+		foreach ($result as $k => $v) {
+			$result[$k]['img'] = 'http://v.yqrtv.com:8080/app/' . $v['img'];
+
+		}
+		echo JSON::encode($result);
+	}
 	public function getTuijian() {
 		$commend_id = IFilter::act(IReq::get('commend_id'), 'int') ? IFilter::act(IReq::get('commend_id'), 'int') : '';
 		var_dump($commend_id);
@@ -120,12 +153,17 @@ class mobileGoods extends IController {
 		$m_goods->page = $page;
 		$m_goods->pagesize = 4;
 		$result = $m_goods->find();
+		foreach ($result as $k => $v) {
+			$result[$k]['img'] = 'http://v.yqrtv.com:8080/app/' . $v['img'];
+
+		}
+
 		echo JSON::encode($result);
-		//var_dump($result);
+
 	}
 	//每个分类下面的商品
 	public function getCatGoods() {
-		$id = IFilter::act(IReq::get('cat_id'), 'int') ? IFilter::act(IReq::get('cat_id'), 'int') : 2;
+		$id = IFilter::act(IReq::get('cat_id')) ? IFilter::act(IReq::get('cat_id')) : 2;
 		$m_category = new IQuery('category');
 		$m_category->fields = 'id,parent_id';
 		//$m_goods->where='id='.$id;
@@ -142,6 +180,10 @@ class mobileGoods extends IController {
 		$m_goods->where = 'c.category_id in (' . $all_id . ')';
 		$m_goods->join = 'left join category_extend as c on go.id=c.goods_id';
 		$result = $m_goods->find();
+		foreach ($result as $k => $v) {
+			$result[$k]['img'] = 'http://v.yqrtv.com:8080/app/' . $v['img'];
+
+		}
 		echo JSON::encode($result);
 
 	}
@@ -150,14 +192,22 @@ class mobileGoods extends IController {
 		$siteConfigObj = new Config("site_config");
 		$site_config = $siteConfigObj->getInfo();
 		$index_slide = isset($site_config['index_slide']) ? unserialize($site_config['index_slide']) : array();
-		echo JSON::encode($index_slide);
+		$tem = array();
+		foreach ($index_slide as $k => $v) {
+			$tem[$k]['img'] = 'http://v.yqrtv.com:8080/app/' . $v['img'];
+		}
+		echo JSON::encode($tem);
 
 	}
 	//获取猜你喜欢的商品信息
 	public function getUserLike() {
-		$user_id = IFilter::act(IReq::get('user_id'), 'int') ? IFilter::act(IReq::get('user_id'), 'int') : 0;
+		$user_id = IFilter::act(IReq::get('user_id')) ? IFilter::act(IReq::get('user_id')) : 0;
 		$likeList = user_like::get_like_cate($user_id, 6);
 		echo JSON::encode($likeList);
+
+	}
+	public function goodsSearch() {
+		$search_list = search_goods::find()->find();
 
 	}
 
