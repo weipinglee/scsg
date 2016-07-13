1871abaeebd19277e5375e8a1a097105e77a83f4
zyn1 <739619279@qq.com>
2016-07-13 15:26:53 +0800

    商家权限

--- controllers/systemseller.php
+++ controllers/systemseller.php
@@ -44,6 +44,7 @@ class SystemSeller extends IController
 				$sellerObj->update($where);
 
 				//存入私密数据
+                ISafe::set('admin_role_seller_name','超级管理员');
 				ISafe::set('seller_id',$sellerRow['id']);
 				ISafe::set('seller_name',$sellerRow['seller_name']);
 				ISafe::set('seller_pwd',$sellerRow['password']);
@@ -58,7 +59,53 @@ class SystemSeller extends IController
 			}
 			else
 			{
-				$message = '用户名与密码不匹配';
+                $adminsellerObj = new IQuery('admin_seller as a');
+                $adminsellerObj->join = 'join seller as s on a.seller_id = s.id';
+                $adminsellerObj->where = 'admin_name = "'.$seller_name.'" and a.is_del = 0 and s.is_del = 0 and s.is_lock = 0';
+                $adminsellerObj->fields = 'a.*';
+                $sellerRow = $adminsellerObj->getObj();
+                if($sellerRow && ($sellerRow['password'] == md5($password)))
+                {
+                    $dataArray = array(
+                        'last_ip'   => IClient::getIp(),
+                        'last_time' => ITime::getDateTime(),
+                    );
+                    $adminObj = new IModel('admin_seller');
+                    $adminObj->setData($dataArray);
+                    $where = 'id = '.$sellerRow["id"];
+                    $adminObj->update($where);
+                    
+                    //根据角色分配权限
+                    if($sellerRow['role_id'] == 0)
+                    {
+                        ISafe::set('admin_role_seller_name','超级管理员');
+                    }
+                    else
+                    {
+                        $roleObj = new IModel('admin_role');
+                        $where   = 'id = '.$sellerRow["role_id"].' and is_del = 0 and seller_id = '.$sellerRow['seller_id'];
+                        $roleRow = $roleObj->getObj($where);
+                        ISafe::set('admin_role_seller_name',$roleRow['name']);
+                    }
+                    ISafe::set('seller_id',$sellerRow['seller_id']);
+                    ISafe::set('admin_seller_id',$sellerRow['id']);
+                    ISafe::set('seller_name',$sellerRow['admin_name']);
+                    ISafe::set('seller_pwd',$sellerRow['password']); 
+                    
+                    if($callbackUrl)
+                    {
+                        $this->redirect("$callbackUrl");
+                    }
+                    else
+                    {
+                        $this->redirect('/seller/index');
+                    }
+                }
+                else
+                {
+                    $message = '用户名与密码不匹配';
+                }
+				
 			}
 		}
 
