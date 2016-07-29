51392226795ca66b85654d8547019c11f3df78d4
zyn1 <739619279@qq.com>
2016-07-29 15:36:51 +0800

    公众号支付调试

--- controllers/site.php
+++ controllers/site.php
@@ -1967,7 +1967,9 @@ class Site extends IController
                 $jsapi_ticket = $result['ticket'];
                 ISafe::set('wxPayJsapiTicket', $jsapi_ticket);
                 var_dump($jsapi_ticket);
-                $str = "jsapi_ticket={$jsapi_ticket}&noncestr={$para['nonceStr']}&timestamp={$para['timeStamp']}&url=http://www.yqtvt.com/test/site/wapPayCode";
+                $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+                $url1 = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                $str = "jsapi_ticket={$jsapi_ticket}&noncestr={$para['nonceStr']}&timestamp={$para['timeStamp']}&url={$url1}";
                 var_dump($str);
                 $this->signature = sha1($str);
                 var_dump($this->signature);
