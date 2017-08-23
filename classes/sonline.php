afa4a0fc2c38b5483176c480f79f1d09cdad5337
weiping <weiping.lee@163.com>
2017-08-23 15:55:52 +0800

    qq客服手机端地址

--- classes/sonline.php
+++ classes/sonline.php
@@ -10,8 +10,9 @@ class Sonline
 {
 	
     private static $qqPcUrl = 'http://wpa.qq.com/msgrd?v=3&uin={$qqNum}&site=qq&menu=yes';
-	private static $qqWapUrl = 'mqqwpa://im/chat?chat_type=wpa&uin={$qqNum}&version=1&src_type=web&web_src=bjhuli.com';
-	
+	//private static $qqWapUrl = 'mqqwpa://im/chat?chat_type=wpa&uin={$qqNum}&version=1&src_type=web&web_src=bjhuli.com';
+	private static $qqWapUrl = 'http://wpa.qq.com/msgrd?v=3&uin={$qqNum}&site=qq&menu=yes';
+
 	//通过qq号获取临时会话url
 	public static function getChatUrl($qq){
         if(IClient::getDevice()=='pc')
