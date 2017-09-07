89322b1ccee24e27599d4dcaf71c90ff8e1f9c3a
weiping <weiping.lee@163.com>
2017-09-07 14:55:30 +0800

    运费问题手机端

--- views/mobile/javascript/orderFormClass.js
+++ views/mobile/javascript/orderFormClass.js
@@ -95,7 +95,7 @@ function orderFormClass()
 	}
 	//获取运费
 	this.get_delivery  = function ()
-	{
+	{//window.realAlert(456);
 		this.deliveryPrice = 0;
 		var orderFormObj = this;
 		var url = this.delivery_fee_url;
@@ -112,11 +112,13 @@ function orderFormClass()
             ,price = 0
             ,_g = []
             ,_group = 0;
+			var _deliveryInfo = '';
             _this.parents('.js_seller_diff').find('.js_goods_delivery').each(function(){
                 var _t = $(this)
                 ,obj = _t.attr('js_data')
                 ,dataArray = obj.split("_");
-                $.ajax({
+				_deliveryInfo = _deliveryInfo + obj + '|';
+               /* $.ajax({
                     type:'post',
                     async:false,
                     data:{"area":area,"deliveryId":dataArray[0],"goodsId":dataArray[1],"productId":dataArray[2],"num":dataArray[3]},
@@ -145,9 +147,35 @@ function orderFormClass()
                         _group = content.group_id;
                     },
                     timeout:1000,
-                })
+                })*/
                 _in++;                                                    
             })
+
+			$.ajax({
+				type: 'post',
+				async:false,
+				data: {"area":area,"delivery_info":_deliveryInfo},
+				url:_get_delivery_url,
+				success : function(data){
+					data  = JSON.parse(data);
+					$.each(data.delivery,function(index){
+						var content = data.delivery[index];
+						//地区无法送达
+						if(content.if_delivery == 1 || content.error == 1)
+						{
+							alert('您选择地区部分商品无法送达');
+						}
+						else{
+							price += (data.delivery[index].price);
+						}
+
+
+					})
+					_g.push(data.goodsList);
+					_group = data.group_id;
+				}
+
+			})
             
             $.ajax({
                 type:'post',
