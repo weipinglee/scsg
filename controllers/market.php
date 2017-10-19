8e2cc76a676d7af7a777c037fea58416ab4f1696
weiping <weiping.lee@163.com>
2017-10-19 12:19:08 +0800

    单日限制次数之表单更改

--- controllers/market.php
+++ controllers/market.php
@@ -517,6 +517,7 @@ class Market extends IController
 			'end_time'      => IFilter::act(IReq::get('end_time','post')),
 			'intro'         => IFilter::act(IReq::get('intro','post'),'text'),
 			'award_type'    => $award_type,
+			'times_day'     => IFilter::act(IReq::get('times_day','post')),
 			'type'          => 0,
 			'user_group'    => $user_group_str,
 			'award_value'   => IFilter::act(IReq::get('award_value','post')),
