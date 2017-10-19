296ec1067eb2599744c971d4a9e9c21aa690677a
weiping <weiping.lee@163.com>
2017-10-19 15:55:51 +0800

    配送员订单详情页

--- controllers/distribution.php
+++ controllers/distribution.php
@@ -92,4 +92,66 @@ class distribution extends IController
 		//print_r($order_list);
 		die(JSON::encode($order_list));
 	}
+
+	/**
+	 * @brief 订单详情
+	 * @return String
+	 */
+	public function order_detail()
+	{
+		$id = IFilter::act(IReq::get('id'),'int');
+		$orderObj = new order_class();
+		$this->order_info = $orderObj->getOrderShow($id);
+		if($this->order_info['type']==4)$this->redirect('preorder_detail/id/'.$this->order_info['id']);
+		$this->fapiao_data = array();
+		if($this->order_info['invoice']==1){
+			$fapiao_db = new IModel('order_fapiao');
+			$this->fapiao_data = $fapiao_db->getObj('order_id='.$id);
+		}
+		if(!$this->order_info)
+		{
+			IError::show(403,'订单信息不存在');
+		}
+
+
+		//获取商品信息
+		$tb_order_goods = new IQuery('order_goods as og');
+		$tb_order_goods->join = 'left join goods as g on og.goods_id=g.id';
+		$tb_order_goods->where = 'og.order_id='.$id;
+		$tb_order_goods->group = 'og.id';
+		$tb_order_goods->fields = 'g.type,g.sell_price,g.point,og.product_id,og.is_send,og.real_price,og.refunds_status,og.id as og_id,og.goods_id,og.img,og.goods_array,og.goods_nums,og.seller_id';
+		$og_data = $tb_order_goods->find();
+		foreach($og_data as $key=>$val){
+			if($val['seller_id'] <> 0)
+			{
+				$seller_name = API::run('getSellerInfo',$val['seller_id'],'true_name');
+				$seller_logo = API::run('getSellerInfo',$val['seller_id'],'logo_img');
+				$og_data[$key]['seller_name'] = $seller_name['true_name'];
+				$og_data[$key]['seller_logo'] = $seller_logo['logo_img'];
+			}
+			else
+			{
+				$og_data[$key]['seller_name'] = '平台自营';
+			}
+			//判断所买商品是否分规格
+			if($val['product_id'])
+			{
+				$product = new IModel('products');
+				$sell_price = $product->getField('id='.$val['product_id'], 'sell_price');
+				$point = $product->getField('id='.$val['product_id'], 'point');
+				$og_data[$key]['sell_price'] = $sell_price ? $sell_price : 0;
+				$og_data[$key]['point'] = $point ? $point : 0;
+			}
+			else
+			{
+				$goods = new IModel('goods');
+				$sell_price = $goods->getField('id='.$val['goods_id'], 'sell_price');
+				$point = $goods->getField('id='.$val['goods_id'], 'point');
+				$og_data[$key]['sell_price'] = $sell_price ? $sell_price : 0;
+				$og_data[$key]['point'] = $point ? $point : 0;
+			}
+		}
+		$this->og_data = $og_data;
+		$this->redirect('order_detail',false);
+	}
 }
